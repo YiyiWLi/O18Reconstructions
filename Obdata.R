ba9ece3ea0d61d90b530471df455d6ade526e613
YiyiWLi <cyiyiwang@gmail.com>
2022-01-26 11:26:01 -0800

    Observation

--- Obdata.R
+++ Obdata.R
@@ -2,22 +2,23 @@
 library(ggplot2)
 library(reshape2)
 
-#load Eigenvalues data
+# load observation data
 load(file="data/OBdata.RData")
 
-time = paste(rep(1997:2005, each = 3), rep(c("-06-15", "-07-15", "-08-15"), 9), sep="")
-colnames(dato) <- c("StationName","GridID","lat","lon",time)
-save(dato, file = "OBdata.RData")
-
-# plot observed data
+# transpose observation data
 obdata<-t(dato[5:31])
+# give station names as column names
 colnames(obdata) <- dato[,1]
+# transform to data frame format
 obdata1<-data.frame(obdata)
 obdata1 <- cbind(rownames(obdata1), data.frame(obdata1, row.names=NULL))
 colnames(obdata1)[1]<-"Time"
 obdata1 <- melt(obdata1, id.vars="Time")
+# change time format from numeric to character 
 obdata1$Time <- as.character(obdata1$Time)
-obdata1$Time <- factor(obdata1$Time, levels = unique(obdata1$Time) )
+# encode time vector as a factor
+obdata1$Time <- factor(obdata1$Time, levels = unique(obdata1$Time))
+# plot observation data 
 ggplot(obdata1, aes(x=Time, value, col=variable)) + geom_point(size=2) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   scale_color_manual(values = c("red", "orange", "green", "blue", "black", "purple",
